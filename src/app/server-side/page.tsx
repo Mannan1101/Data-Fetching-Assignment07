0d2ab0a613e0496ac7a92d37a4e20aa142b996ea
Abdul Mannan <161303446+Mannan1101@users.noreply.github.com>
2025-01-03 02:12:36 +0500

    Update page.tsx

--- src/app/server-side/page.tsx
+++ src/app/server-side/page.tsx
@@ -1,5 +1,5 @@
 import Header from '../components/Header'
-import Server from '../components/ServerSide/Server'
+import Server from '../components/Serverside/Server'
 import React from 'react'
 
 export default function Page() {
